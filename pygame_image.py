5a290ab92a21e109f84f036c93510cef3a452309
kurata <c0a23054f3@edu.teu.ac.jp>
2024-04-16 16:31:55 +0900

    練習6

--- pygame_image.py
+++ pygame_image.py
@@ -17,7 +17,8 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [-tmr%800, 0])
+        x = tmr%800
+        screen.blit(bg_img, [-x, 0])
         screen.blit(kk_ton, [300, 200])
         pg.display.update()
         tmr += 1        
